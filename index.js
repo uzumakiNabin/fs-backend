aaa4a3769006601a1133932d4a97d5c4db2d975d
uzumakiNabin <nabin.kaucha@gmail.com>
2023-02-22 22:41:42 +0545

    ready for deploy

--- index.js
+++ index.js
@@ -90,6 +90,6 @@ app.post("/api/persons", (request, response) => {
   response.status(201).json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
