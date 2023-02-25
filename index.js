2940fd1bb3b60e781abff80b8b3ec1d0c1ea72e3
uzumakiNabin <nabin.kaucha@gmail.com>
2023-02-25 19:35:04 +0545

    part 3 ex 3.15 - 3.18

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
+//--------------custom middlewares--------------
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
@@ -19,44 +20,47 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "invalid id format" });
+  }
+  next(error);
+};
+
+//--------------api endpoints--------------
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((people) => {
     response.json(people);
   });
 });
 
-// app.get("/info", (request, response) => {
-//   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`);
-// });
-
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   Person.findById(id)
     .then((person) => {
-      response.json(person);
+      if (person) response.json(person);
+      else response.status(404).end();
     })
-    .catch((err) => response.status(400).end());
+    .catch((err) => {
+      next(err);
+    });
 });
 
-// app.delete("/api/persons/:id", (request, response) => {
-//   const id = request.params.id;
-//   if (persons.findIndex((person) => person.id === +id) > -1) {
-//     persons = persons.filter((person) => person.id !== +id);
-//     response.status(204).end();
-//   } else {
-//     response.status(404).end();
-//   }
-// });
+app.delete("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  Person.findByIdAndRemove(id)
+    .then((result) => response.status(204).end())
+    .catch((err) => next(err));
+});
+
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
   if (!body.name || body.name === "" || body.name === null) {
-    response.status(400).json({ error: "name is required" });
-    return;
+    return response.status(400).json({ error: "name is required" });
   }
   if (!body.number || body.number === "" || body.number === null) {
-    response.status(400).json({ error: "number is required" });
-    return;
+    return response.status(400).json({ error: "number is required" });
   }
   Person.findOne({ name: body.name })
     .then((existingPerson) => {
@@ -67,10 +71,30 @@ app.post("/api/persons", (request, response) => {
         person.save().then((savedPerson) => response.json(savedPerson));
       }
     })
-    .catch((err) => response.status(400).end());
+    .catch((err) => response.status(500).end());
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  const body = request.body;
+
+  if (!body.name || body.name === "" || body.name === null) {
+    return response.status(400).json({ error: "name is required" });
+  }
+  if (!body.number || body.number === "" || body.number === null) {
+    return response.status(400).json({ error: "number is required" });
+  }
+
+  const person = { name: body.name, number: body.number };
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((err) => next(err));
 });
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
